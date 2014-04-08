ad0dc5e32b98c69cba6263b5483d6bbec0080045
David Conner <dconner.pro@gmail.com>
2014-04-08 16:28:50 -0400

    set lineman to compile bootstrap/jquery

--- config/files.js
+++ config/files.js
@@ -9,15 +9,18 @@ module.exports = function(lineman) {
   //Override file patterns here
   return {
 
-    // As an example, to override the file patterns for
-    // the order in which to load third party JS libs:
-    //
-    // js: {
-    //   vendor: [
-    //     "vendor/js/underscore.js",
-    //     "vendor/js/**/*.js"
-    //   ]
-    // }
+    js: {
+      vendor: [
+        "vendor/bower/jquery/dist/jquery.js",
+        "vendor/bower/bootstrap/dist/js/bootstrap.js"
+      ]
+    },
+
+    css: {
+      vendor: [
+        "vendor/bower/bootstrap/dist/css/bootstrap.css"
+      ]
+    }
 
   };
 };
